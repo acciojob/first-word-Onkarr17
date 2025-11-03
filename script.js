1742f01f9138697b394e51349bce35d5606c4912
Onkarr17 <onkarhadgaonkar22@gmail.com>
2025-11-04 00:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	s= s.trim();
+	return s.split(" ")[0];
 }
 
-// Do not change the code below
+// Do not change the code belo
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
